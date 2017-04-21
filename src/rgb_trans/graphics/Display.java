ebf6b20b8381dfc2e18b1c359c47dffa76d18982
CathbadIII <bckwalter44@gmail.com>
2017-04-20 19:41:54 -0500

    Basic Display Class Functionality

--- src/rgb_trans/graphics/Display.java
+++ src/rgb_trans/graphics/Display.java
@@ -1,5 +1,9 @@
 package rgb_trans.graphics;
 
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
 public class Display {
 
 	/*
@@ -7,4 +11,54 @@ public class Display {
 	 * should contain all of the
 	 * components of the program.
 	 */
+	private static final int UPS = 60;
+	private int WIDTH;
+	private int HEIGHT;
+	private static final long NANO = 1000000000;
+	
+	private static DrawCanvas canvas;
+	
+	public Display(int WIDTH, int HEIGHT){
+		this.WIDTH = WIDTH;
+		this.HEIGHT = HEIGHT;
+	}
+	
+	public void drawMain(){
+		canvas = new DrawCanvas(this.WIDTH, this.HEIGHT, Color.BLACK);
+		
+		JFrame frame = new JFrame("Title");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(canvas);
+		frame.setLocationByPlatform(true);
+		frame.pack();
+		frame.setVisible(true);
+		
+		canvas.createBufferStrategy(2);
+		run();
+	}
+	
+	private static void render(){
+		canvas.ready();
+		canvas.clear();
+		canvas.cleanUp();
+	}
+	
+	private static void run(){
+		boolean running = true;
+		long lastUpTime = System.nanoTime();
+		long lastSecTime = System.nanoTime();
+		int ups = 0;
+		while(running){
+			if(System.nanoTime() - lastUpTime > NANO/UPS){
+				render();
+				lastUpTime = System.nanoTime();
+				ups++;
+			}
+			if(System.nanoTime() - lastSecTime > NANO){
+				//System.out.println(ups);
+				lastSecTime = System.nanoTime();
+				ups = 0;
+			}
+		}
+	}
 }
--- src/rgb_trans/test/TestMainCathbad.java
+++ src/rgb_trans/test/TestMainCathbad.java
@@ -1,8 +1,11 @@
 package rgb_trans.test;
 
+import rgb_trans.graphics.Display;
+
 public class TestMainCathbad {
 
 	public static void main(String... args){
-		
+		Display display = new Display(1920, 1080);
+		display.drawMain();
 	}
 }
