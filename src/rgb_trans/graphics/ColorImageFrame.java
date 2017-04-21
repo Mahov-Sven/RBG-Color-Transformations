d444c7df7740cd9540b8e8dd1d80df3611763a51
FireEmber <empenn54@gmail.com>
2017-04-21 14:49:47 -0500

    Completed ColorImageFrame.
    Dispalys an array of pixels defined in its pixel array.

--- src/rgb_trans/graphics/ColorImageFrame.java
+++ src/rgb_trans/graphics/ColorImageFrame.java
@@ -9,101 +9,199 @@ import java.awt.event.MouseWheelEvent;
 
 import javax.swing.JPanel;
 
-public class ColorImageFrame extends JPanel{
+public class ColorImageFrame extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	private static final int BORDER_SIZE = 10;
+	private static final int SELECTED_BORDER_SIZE = 8;
+
 	private int[] pixels;
-	private int width, height;
+	private int arrayWidth, arrayHeight;
 	
-	private int pixelWidth;
+	private int pixelSize;
 	private int windowX, windowY;
 	private int selected;
 
 	public ColorImageFrame(int width, int height) {
 		this.pixels = new int[0];
-		this.width = 0;
-		this.height = 0;
-		this.pixelWidth = 32;
+		this.arrayWidth = 0;
+		this.arrayHeight = 0;
+		this.pixelSize = 32;
 		this.windowX = 100;
 		this.windowY = 100;
 		this.selected = -1;
-		
+
 		setPreferredSize(new Dimension(width, height));
-		
+
 		MouseAdapter mouseAdapter = new MouseAdapter() {
 			private boolean dragged = false;
 			private int startX, startY;
-			
-            public void mouseDragged(MouseEvent e){
-            	System.out.println("hello");
-            	if(!dragged){
-            		startX = e.getX();
-            		startY = e.getY();
-            		dragged = true;
-            	}
-            	moveImage(e.getX() - startX, e.getY() - startY);
-            	startX = e.getX();
-        		startY = e.getY();
-            }
-            
-            public void mouseReleased(MouseEvent e){
-            	dragged = false;
-            	System.out.println("hello");
-            }
-            
-            public void mouseWheelMoved(MouseWheelEvent e){
-            	zoomImage(e.getWheelRotation());
-            }
-        };
-		
+
+			public void mouseClicked(MouseEvent e) {
+				clickSelected(e.getX(), e.getY());
+			}
+
+			public void mouseDragged(MouseEvent e) {
+				if (dragged) {
+					moveImage(e.getX() - startX, e.getY() - startY);
+					startX = e.getX();
+					startY = e.getY();
+				}
+			}
+
+			public void mousePressed(MouseEvent e) {
+				if (e.getX() < windowX - BORDER_SIZE || e.getY() < windowY - BORDER_SIZE
+						|| e.getX() > windowX + BORDER_SIZE + arrayWidth * pixelSize
+						|| e.getY() > windowY + BORDER_SIZE + arrayHeight * pixelSize)
+					return;
+
+				dragged = true;
+				startX = e.getX();
+				startY = e.getY();
+			}
+
+			public void mouseReleased(MouseEvent e) {
+				dragged = false;
+			}
+
+			public void mouseWheelMoved(MouseWheelEvent e) {
+				zoomImage(e.getWheelRotation(), e.getX(), e.getY());
+			}
+		};
+
 		addMouseListener(mouseAdapter);
 		addMouseMotionListener(mouseAdapter);
 		addMouseWheelListener(mouseAdapter);
 	}
 	
-	public void zoomImage(int deltaZ){
-		double deltaZoom = Math.pow(2, deltaZ);
-    	pixelWidth *= deltaZoom;
-    	
-    	if(pixelWidth < 1)
-    		pixelWidth = 1;
-    	
-    	if(pixelWidth > 256)
-    		pixelWidth = 256;
-    	
-    	repaint();
-	}
-	
-	public void moveImage(int deltaX, int deltaY){
-		this.windowX += deltaX;
-		this.windowY += deltaY;
-		System.out.println("working");
-		repaint();
-	}
-	
-	public void setPixels(int[] pixels, int width, int height){
-		this.pixels = pixels;
-		this.width = width;
-		this.height = height;
+	private void clickSelected(int x, int y){
+		int pixelX = selected % arrayWidth;
+		int pixelY = selected / arrayHeight;
+
+		if (x < windowX || y < windowY || x > windowX + arrayWidth * pixelSize
+				|| y > windowY + arrayWidth * pixelSize
+				|| (x > pixelX && y > pixelY && x < pixelX + pixelSize && y < pixelY + pixelSize)) {
+			selected = -1;
+		} else {
+			x -= windowX;
+			y -= windowY;
+			x /= pixelSize;
+			y /= pixelSize;
+			
+			selected = y * arrayWidth + x;
+		}
 		
 		repaint();
 	}
 	
-	public void setPixel(int x, int y, Color color){
-		pixels[y * width + x] = (color.getRed() << 16 + color.getGreen() << 8 + color.getBlue());
-		
-		repaint();
+	public Color getSelectedColor(){
+		return new Color(pixels[selected]);
 	}
 	
-	public void paintComponent(Graphics g){
+	public int getSelectedX(){
+		return selected % arrayWidth;
+	}
+	
+	public int getSelectedY(){
+		return selected / arrayHeight;
+	}
+	
+	public void moveImage(int deltaX, int deltaY) {
+		windowX += deltaX;
+		windowY += deltaY;
+
+		if (windowX < 0)
+			windowX = 0;
+
+		if (windowX > getWidth())
+			windowX = getWidth();
+
+		if (windowY < 0)
+			windowY = 0;
+
+		if (windowY > getHeight())
+			windowY = getHeight();
+
+		repaint();
+	}
+
+	public void paintComponent(Graphics g) {
 		g.setColor(Color.DARK_GRAY);
 		g.fillRect(0, 0, getWidth(), getHeight());
-		
-		for(int y = 0; y < height; y++){
-			for(int x = 0; x < width; x++){
-				g.setColor(new Color(pixels[y * width + x]));
-				g.fillRect(windowX + x * pixelWidth, windowY + y * pixelWidth, pixelWidth, pixelWidth);
+
+		g.setColor(Color.GRAY);
+		g.fillRect(windowX - BORDER_SIZE, windowY - BORDER_SIZE, arrayWidth * pixelSize + 2 * BORDER_SIZE,
+				arrayHeight * pixelSize + 2 * BORDER_SIZE);
+
+		for (int y = 0; y < arrayHeight; y++) {
+			for (int x = 0; x < arrayWidth; x++) {
+				if(selected == y * arrayWidth + x)
+					continue;
+				
+				g.setColor(new Color(pixels[y * arrayWidth + x]));
+				g.fillRect(windowX + x * pixelSize, windowY + y * pixelSize, pixelSize, pixelSize);
 			}
 		}
+		
+		if(selected >= 0){
+			int x = selected % arrayWidth;
+			int y = selected / arrayHeight;
+			
+			g.setColor(Color.LIGHT_GRAY);
+			g.fillRect(windowX - SELECTED_BORDER_SIZE + x * pixelSize, windowY - SELECTED_BORDER_SIZE + y * pixelSize, pixelSize + 2 * SELECTED_BORDER_SIZE, pixelSize + 2 * SELECTED_BORDER_SIZE);
+		
+			g.setColor(new Color(pixels[selected]));
+			g.fillRect(windowX + x * pixelSize, windowY + y * pixelSize, pixelSize, pixelSize);
+		}
+	}
+
+	public boolean pixelSelected(){
+		return selected > -1 ? true : false;
+	}
+
+	public void setPixel(int x, int y, Color color) {
+		pixels[y * arrayWidth + x] = (color.getRed() << 16 + color.getGreen() << 8 + color.getBlue());
+
+		repaint();
+	}
+
+	public void setPixels(int[] pixels, int width, int height) {
+		this.pixels = pixels;
+		this.arrayWidth = width;
+		this.arrayHeight = height;
+
+		repaint();
+	}
+
+	public void zoomImage(int deltaZ, int x, int y) {
+		deltaZ *= -1;
+
+		if ((pixelSize <= 1 && deltaZ < 0) || (pixelSize >= 256 && deltaZ > 0))
+			return;
+
+		/* Moving window to proper location */
+		x -= windowX;
+
+		if (x < 0)
+			x = 0;
+
+		if (x > arrayWidth * pixelSize)
+			x = arrayWidth * pixelSize;
+
+		y -= windowY;
+
+		if (y < 0)
+			y = 0;
+
+		if (y > arrayHeight * pixelSize)
+			y = arrayHeight * pixelSize;
+
+		windowX -= x * (3 * deltaZ + 1) / 4;
+		windowY -= y * (3 * deltaZ + 1) / 4;
+
+		/* Changing Pixel Size */
+		pixelSize *= Math.pow(2, deltaZ);
+
+		repaint();
 	}
 }
