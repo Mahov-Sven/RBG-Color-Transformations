ca4edacda78f3e46fe39c63d1e098d58be1f50d0
CathbadIII <bckwalter44@gmail.com>
2017-04-23 15:16:43 -0500

    Makes the screen size in test correct.

--- src/rgb_trans/test/TestMainCathbad.java
+++ src/rgb_trans/test/TestMainCathbad.java
@@ -1,11 +1,13 @@
 package rgb_trans.test;
 
+import java.awt.Toolkit;
+
 import rgb_trans.graphics.Display;
 
 public class TestMainCathbad {
 
 	public static void main(String... args){
-		Display display = new Display(1920, 1080);
+		Display display = new Display(Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
 		display.drawMain();
 	}
 }
